36127effd36fef97a9058181a03088e98a64c73b
Jones-Thomas <jonesthomas.work@gmail.com>
2023-04-22 17:54:31 +0530

    Initial Commit

---
+++ src/main/java/com/springbootdemo/helloworld/HelloworldApplication.java
@@ -0,0 +1,24 @@
+package com.springbootdemo.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
+
+@RestController
+@RequestMapping(path = "/")
+class DemoController{
+	@GetMapping("print/{msg}")
+	public String print(@PathVariable(value = "") String msg){
+		return "Helloo "+msg;
+	}
+
+}
